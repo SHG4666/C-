4135a26fc3f67ac24a1ad987793cabde17eab946
SHG4666 <3120591307@qq.com>
2022-06-29 21:53:54 +0800

    添加项目文件。

---
+++ 02_函数重载/main.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+
+//函数重载
+//int sum(int a, int b) {
+//	return a + b;
+//}
+//int sum(int a, int b, int c) {
+//	return a + b + c;
+//}
+//void func(int a, double b) {
+//	cout << "func(int a, double b)" << endl;
+//}
+//void func(double a,int b) {
+//	cout << "func(double a, int b)" << endl;
+//}
+
+//会报错，无法区分仅返回值不同的函数
+//int func() {
+//	return 0;
+//}
+//double func() {
+//	return 1.0;
+//}
+
+void display() {
+	cout << "display()" << endl;
+}
+//display_int
+void display(int a) {
+	cout << "display(int a)" <<a<< endl;
+}
+
+//display_int
+void display(double a) {
+	cout << "display(double a)" <<a<< endl;
+}
+//display_long
+void display(long a) {
+	cout << "display(long a)" <<a<< endl;
+}
+int main() {
+	/*cout << sum(10, 20) << endl;
+	cout << sum(10, 20, 30) << endl;*/
+	/*func(10, 10.0);
+	func(10.0, 10);*/
+//	func();
+	display();
+	display(10);
+	display(10L);
+	display(10.0);
+}
